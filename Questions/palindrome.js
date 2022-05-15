1c748589562b0d86fcf7ec2f37e763003e9c2b13
fuse28 <mixfuse92@gmail.com>
2022-05-15 09:57:30 +0530

    finding if a number is palindrome or not

---
+++ Questions/palindrome.js
@@ -0,0 +1,24 @@
+// let x = 123;
+
+// let duplicate = 123;
+// let reverse = 0;
+// while (duplicate > 0) {
+//   let lastDigit = duplicate % 10;
+//   reverse = reverse * 10 + lastDigit;
+//   duplicate = Math.floor(duplicate / 10);
+// }
+// if (reverse > 0 || reverse % 10 != 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+var isPalindrome = function (x) {
+  let duplicate = x;
+  let reverse = 0;
+  while (duplicate > 0) {
+    let lastDigit = duplicate % 10;
+    reverse = reverse * 10 + lastDigit;
+    duplicate = Math.floor(duplicate / 10);
+  }
+  return x == reverse;
+};
