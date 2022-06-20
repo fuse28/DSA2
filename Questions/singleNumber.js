9fb5d27547b39096f31bbfedb75c9918cc8a2469
fuse28 <mixfuse92@gmail.com>
2022-06-20 16:44:16 +0530

    find the number that apprear once in an array

---
+++ Questions/singleNumber.js
@@ -0,0 +1,30 @@
+//Q) Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+// Example 1:
+// Input: nums = [2,2,1]
+// Output: 1
+
+// Example 2:
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+
+// Example 3:
+// Input: nums = [1]
+// Output: 1
+
+function singleNumber(nums) {
+  let set = new Set();
+
+  for (let num of nums) {
+    if (set.has(num)) {
+      set.delete(num);
+    } else {
+      set.add(num);
+    }
+  }
+  for (let num of set) {
+    return num; // [4,1,2,1,2]  //! ans=1
+  }
+}
