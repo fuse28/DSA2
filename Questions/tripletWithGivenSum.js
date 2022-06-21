370f9de11872a210d7aec6eac7eb85a52fb337fb
fuse28 <mixfuse92@gmail.com>
2022-06-21 18:56:22 +0530

    find the triplet in an array for given sum

--- Questions/tripletWithGivenSum.js
+++ Questions/tripletWithGivenSum.js
@@ -6,3 +6,26 @@
 
 // Output
 // 1,2,5
+
+function tripletSum(arr, sum) {
+  let firstEle;
+  let lastEle;
+
+  arr.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length - 2; i++) {
+    firstEle = i + 1;
+    lastEle = arr.length - 1;
+
+    while (firstEle < lastEle) {
+      if (arr[i] + arr[firstEle] + arr[lastEle] === sum) {
+        console.log(arr[i] + "," + arr[firstEle] + "," + arr[lastEle]);
+      } else if (arr[i] + arr[firstEle] + arr[lastEle] < sum) {
+        firstEle++;
+      } else {
+        lastEle--;
+      }
+    }
+  }
+  console.log("no triplet found for given sum");
+}
+tripletSum([1, 2, 5, 6, 7, 8, 3], 8); //ans=1,2,5
