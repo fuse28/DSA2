0ea935556e1f8168babbfd65cf097ad73cd5b311
fuse28 <mixfuse92@gmail.com>
2022-07-11 18:26:46 +0530

    check in both string if the char is available

---
+++ Questions/ransomNote.js
@@ -0,0 +1,35 @@
+//Q) Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+// Each letter in magazine can only be used once in ransomNote.
+
+// Example 1:
+// Input: ransomNote = "a", magazine = "b"
+// Output: false
+
+// Example 2:
+// Input: ransomNote = "aa", magazine = "ab"
+// Output: false
+
+// Example 3:
+// Input: ransomNote = "aa", magazine = "aab"
+// Output: true
+
+var canConstruct = function (ransomNote, magazine) {
+  const magazineMap = {};
+
+  for (const char of magazine) {
+    if (char in magazineMap) {
+      magazineMap[char] += 1;
+    } else {
+      magazineMap[char] = 1;
+    }
+  }
+  for (const char of ransomNote) {
+    if (char in magazineMap && magazineMap[char] !== 0) {
+      magazineMap[char] -= 1;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
